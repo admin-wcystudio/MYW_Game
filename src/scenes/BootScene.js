439a0cbe1ef3275192bde18508924949f8baf990
YoyoDev0424 <admin@wcystudio.com>
2026-02-09 11:39:13 +0800

    add mov format

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -60,7 +60,7 @@ export class BootScene extends Phaser.Scene {
         this.load.image('save_btn', 'assets/Settings/setting_page_save.png');
         this.load.image('save_btn_click', 'assets/Settings/setting_page_save_click.png');
 
-
+        //login page
         const loginPath = 'assets/Login/';
         this.load.video('login_bg_video', loginPath + 'choosepage_bg.mp4');
 
@@ -74,12 +74,31 @@ export class BootScene extends Phaser.Scene {
         this.load.image('bubble1', loginPath + 'choosepage_bubble1.png');
         this.load.image('bubble2', loginPath + 'choosepage_bubble2.png');
 
-        this.load.video('boy_chinese', loginPath + 'choosepage_boy_chinese.webm');
-        this.load.video('girl_chinese', loginPath + 'choosepage_girl_chinese.webm');
-        this.load.video('boy_galaxy', loginPath + 'choosepage_boy_galaxy.webm');
-        this.load.video('girl_galaxy', loginPath + 'choosepage_girl_galaxy.webm');
-        this.load.video('boy_transition', loginPath + 'choosepage_boy_galaxytochinese_transition.webm');
-        this.load.video('girl_transition', loginPath + 'choosepage_girl_galaxytochinese_transition.webm');
+        this.load.video('boy_chinese', [
+            loginPath + 'choosepage_boy_chinese.webm',
+            loginPath + 'choosepage_boy_chinese.mov'
+        ]);
+
+        this.load.video('girl_chinese', [
+            loginPath + 'choosepage_girl_chinese.webm',
+            loginPath + 'choosepage_girl_chinese.mov'
+        ]);
+        this.load.video('boy_galaxy', [
+            loginPath + 'choosepage_boy_galaxy.webm',
+            loginPath + 'choosepage_boy_galaxy.mov'
+        ]);
+        this.load.video('girl_galaxy', [
+            loginPath + 'choosepage_girl_galaxy.webm',
+            loginPath + 'choosepage_girl_galaxy.mov'
+        ]);
+        this.load.video('boy_transition', [
+            loginPath + 'choosepage_boy_galaxytochinese_transition.webm',
+            loginPath + 'choosepage_boy_galaxytochinese_transition.mov'
+        ]);
+        this.load.video('girl_transition', [
+            loginPath + 'choosepage_girl_galaxytochinese_transition.webm',
+            loginPath + 'choosepage_girl_galaxytochinese_transition.mov'
+        ]);
         this.load.video('transition', loginPath + 'transition.webm');
 
         //main street
